91ec3bfe665234e7a6d04868d867d7ebc03378d8
Mirba Sapar <163499285+mirbasapar@users.noreply.github.com>
2024-04-04 22:45:01 +0600

    pic_random

--- main.py
+++ main.py
@@ -4,36 +4,36 @@ from aiogram.filters import Command
 from dotenv import load_dotenv
 from os import getenv
 import logging
-import random
+from random import choice
+import os
+from aiogram.types import FSInputFile
 
 
-load_dotenv
+load_dotenv()
 bot = Bot(token=getenv('BOT_TOKEN'))
 dp = Dispatcher()
 
-#handlers
 
+#handlers
 @dp.message(Command('start'))
 async def start_cmd(message: types.Message):
-    await message.answer(f'Hello!, {message.from_user.first_name}')
+    await message.answer(f'Привет!, {message.from_user.first_name}')
+
 
 @dp.message(Command('myinfo'))
 async def send_myinfo(message: types.Message):
     await message.answer(f"Your ID: {message.from_user.id}\n"
                          f"Your first name: {message.from_user.first_name}\n"
                          f"Your nic name: {message.from_user.username}")
-    
-@dp.message(Command('picture'))
-async def send_random_pic(message: types.Message):
-    images_dir = 'images'
-    image_files = types.FSInputFile(random(images_dir))
-    await message.answer_photo(photo=image_files)
 
 
-# @dp.message(Command('picture'))
-# async def send_picture(message: types.Message):
-#     file = types.FSInputFile('images/python.jpeg')
-#     await message.answer_photo(photo=file)
+@dp.message(Command('pic_random'))
+async def pic_random(message: types.Message):
+    photos = os.listdir('images/')
+    path = f'images/{choice(photos)}'
+    file = FSInputFile(path)
+    await message.answer_photo(photo=file, caption='красивая фотка')
+
 
 @dp.message()
 async def echo(message: types.Message):
@@ -44,7 +44,7 @@ async def main():
     # start bot
     await dp.start_polling(bot)
 
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
+
