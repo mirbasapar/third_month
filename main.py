0e032b8562e1bf75b5fbcd6fde12553c6b6aab5d
Mirba Sapar <163499285+mirbasapar@users.noreply.github.com>
2024-04-08 01:14:22 +0600

    Handlers, keyboards, init

--- main.py
+++ main.py
@@ -1,47 +1,18 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from dotenv import load_dotenv
-from os import getenv
 import logging
-from random import choice
-import os
-from aiogram.types import FSInputFile
+from bot import bot, dp
+from handlers import start_router, pic_router, callback_router, echo_router, menu_router
 
-
-load_dotenv()
-bot = Bot(token=getenv('BOT_TOKEN'))
-dp = Dispatcher()
-
-
-#handlers
-@dp.message(Command('start'))
-async def start_cmd(message: types.Message):
-    await message.answer(f'Привет!, {message.from_user.first_name}')
-
-
-@dp.message(Command('myinfo'))
-async def send_myinfo(message: types.Message):
-    await message.answer(f"Your ID: {message.from_user.id}\n"
-                         f"Your first name: {message.from_user.first_name}\n"
-                         f"Your nic name: {message.from_user.username}")
-
-
-@dp.message(Command('pic_random'))
-async def pic_random(message: types.Message):
-    photos = os.listdir('images/')
-    path = f'images/{choice(photos)}'
-    file = FSInputFile(path)
-    await message.answer_photo(photo=file, caption='красивая фотка')
-
-
-@dp.message()
-async def echo(message: types.Message):
-    logging.info(message.text)
-    await message.answer(message.text)
 
 async def main():
-    # start bot
+    dp.include_router(start_router)
+    dp.include_router(pic_router)
+    dp.include_router(callback_router)
+    dp.include_router(menu_router)
+
+
+    dp.include_router(echo_router)
+    
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
